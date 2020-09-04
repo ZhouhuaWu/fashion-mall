0f1aaa06e3492f98a6c2459a0c5ec9c55ac576c4
zhouhua_wu <1610291@gmail.com>
2020-09-04 17:38:32 +0800

    首页开发

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="detail">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Detail',
+  data() {
+    return {
+
+    }
+  },
+
+  created(){
+
+
+  },
+  methods:{
+
+  },
+
+  components:{
+
+  }
+ }
+</script>
+
+<style scoped>
+  #detail {
+    height: 100vh;
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+
+  .content {
+    position: absolute;
+    top: 44px;
+    bottom: 60px;
+  }
+  .detail-nav{
+    position: relative;
+    z-index: 9;
+    background: #fff;
+  }
+
+  .back-top {
+    position: fixed;
+    right: 10px;
+    bottom: 65px;
+  }
+</style>
---
+++ src/views/shopcart/Shopcart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="shopcart">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Shopcart',
+  data() {
+    return {
+
+    }
+  },
+  components:{
+
+  },
+  computed:{
+
+  }
+ }
+</script>
+
+<style  scoped>
+  #shopcart {
+    /*position: relative;*/
+    height: 100vh;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+
+  .cart-list {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    width: 100%;
+  }
+</style>
